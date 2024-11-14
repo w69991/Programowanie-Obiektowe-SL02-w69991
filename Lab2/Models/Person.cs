d5d4fa259777b5eaea16b91371e5f91970f34216
Adam Orłowski <w69991@student.wsiz.edu.pl>
2024-11-14 11:15:59 +0100

    Zmiana

---
+++ Lab2/Models/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Models
+{
+    public class Osoba
+    {
+        private string Imie;
+        private string Nazwisko;
+        private int Wiek;
+        private string Pesel;
+            
+        public Osoba(string imie, string nazwisko, int wiek, string pesel)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Wiek = wiek;
+            Pesel = pesel;
+        }
+    public string imie
+        { get { return Imie; }
+          set { Imie = value; }
+        }
+    public string nazwisko
+            { get { return Nazwisko; } set { Nazwisko = value; } }
+    public int wiek
+            { get { return Wiek; } set { Wiek = value; } }
+
+     public readonly string pesel;
+    
+    public void PrzedstawSie()
+        {
+            Console.WriteLine($"Nazywam sie {imie} {nazwisko} i mam {wiek} lat ");
+        }
+    }
+}
