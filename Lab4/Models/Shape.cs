c0a4af42ce3a07ecf17514f25ece885defc78e2d
Adam Orłowski <w69991@student.wsiz.edu.pl>
2024-11-28 11:16:12 +0100

    Lab4

---
+++ Lab4/Models/Shape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab4.Models
+{ 
+	internal class Shape
+	{
+		public double X { get; set; }
+		public double Y { get; set; }
+		public double Height { get; set; }
+		public double Width { get; set; }
+
+
+
+        public virtual void Draw()
+		{
+			Console.WriteLine(nameof(Shape));
+		}
+	}	
+}
